20025ecd89a4037dd72785d6e3e058f2815af014
BrianPeluche <brianpeluche10@hotmail.com>
2025-10-27 21:20:48 -0700

    added files

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+
+print("\nWelcome to XXX bgarc208/hwheeler 8 puzzle solver. Type “1” \nto use a default puzzle, or “2” to enter your own puzzle.\n")
+
+user_input = int(input("Please enter a number: "))
+    
+while user_input != 1 and user_input != 2 :
+    print("Incorrect. ")
+    user_input = int(input("Please enter a number: "))
