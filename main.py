09ddafc59c97a8d3d108db24225d6d50bb743367
BrianPeluche <brianpeluche10@hotmail.com>
2025-10-28 16:24:08 -0700

    added prompt

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ user_input = int(input("Please enter a number: "))
 while user_input != 1 and user_input != 2 :
     print("Incorrect. ")
     user_input = int(input("Please enter a number: "))
+
+print("Enter your puzzle, use a zero to represent the blank")
+row_one = list(map(int, input("Enter the first row, use space or tabs between numbers  ").split()))
+row_two = list(map(int, input("Enter the second row, use space or tabs between numbers  ").split()))
+row_three = list(map(int, input("Enter the third row, use space or tabs between numbers  ").split()))
+
+print(row_one)
+print(row_two)
+print(row_three)
+print("\nEnter your choice of algorithm")
+print("\n1) Uniform Cost Search\n2) A* with the Misplaced Tile heuristic.\n3) A* with the Euclidean distance heuristic.\n")
