21705ced03acb5bfc34e42753803c14b162281f4
Howard Wheeler <167825355+Howardwheeler@users.noreply.github.com>
2025-10-30 22:29:25 -0700

    uniform search fixes (still needs more)

--- puzzle/tilePuzzle.py
+++ puzzle/tilePuzzle.py
@@ -1,18 +1,17 @@
-class puzzleState:
+class PuzzleState:
 
     # Constructor to initialize the puzzle state
-    def init(self, board, emptyTile, numMoves, prevState, boardSize): 
+    def __init__(self, board, emptyTile, prevState, boardSize, direction): 
         self.board = board
         self.emptyTile = emptyTile
-        self.numMoves = numMoves
         self.prevState = prevState
         self.boardSize = boardSize
+        self.direction = direction
 
     # Method to find the position of the empty tile (0)
     def findEmptyTile(self):
-        self.board.index(0)
         return self.board.index(0)
-    
+
     # Method to get the (row, col) position of the empty tile
     def emptyTilePos(self, size):
         row = self.emptyTile // size
@@ -27,7 +26,7 @@ class puzzleState:
     
     # Method to move the empty tile in a specified direction
     def moveTile(self, direction, size):
-        row, col = self.emptyTilePos()
+        row, col = self.emptyTilePos(size)
         newRow, newCol = row, col
 
         if direction == 'up':
@@ -45,8 +44,13 @@ class puzzleState:
             newEmptyTile = newRow * size + newCol
             newBoard = self.board[:]
             newBoard[self.emptyTile], newBoard[newEmptyTile] = newBoard[newEmptyTile], newBoard[self.emptyTile]
-            return puzzleState(newBoard, newEmptyTile, self.numMoves + 1, self, size)
+            return PuzzleState(newBoard, newEmptyTile, self, size, direction)
         else:
             return None
         
+    # Method to display the board state
+    def printBoard(self):
+        size = self.boardSize
+        for i in range(size):
+            print(self.board[i*size:(i+1)*size])
         
